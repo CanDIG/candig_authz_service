b9065937991516dc1b0ba9da753020845b910dc1
Jimmy Li <lihaoyuan1996@gmail.com>
2020-04-03 12:45:22 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# candig_authz_service
-CanDIG Authorization Service.
+
+# Authz Service
+
+Implementation of CanDIG Authorization Service.
+
+## Installation
+
+The Authz service can be installed in a py3.7+ virtual enviroment:
+```
+pip install -r requirements.txt
+```
+
+### Running
+
+The service can be started with:
+
+```
+python -m candig_authz_service
+```
+
+### Testing
+
+Tests can be run with pytest and coverage:
+```
+pytest --cov=candig_authz_service tests/
+```
---
+++ candig_authz_service/tools/ingester.py
@@ -0,0 +1,79 @@
+"""
+The ingester module provides methods to ingest tsv files
+"""
+
+import os
+import sys
+
+from sqlalchemy.exc import IntegrityError
+
+sys.path.append(os.getcwd())
+
+from candig_authz_service.api.exceptions import FileTypeError  # noqa: E402
+from candig_authz_service.api.exceptions import KeyExistenceError  # noqa: E402
+from candig_authz_service.api.exceptions import HeaderError  # noqa: E402
+from candig_authz_service.orm import init_db, get_engine  # noqa: E402
+from candig_authz_service.orm.models import authorization  # noqa: E402
+
+
+class Ingester:
+    """
+
+    """
+    def __init__(self, database, patient, sample, tsv_file):
+        """Constructor method
+        """
+        self.db = "sqlite:///" + database
+        self.file_type = self.get_type()
+        self.engine = self.db_setup()
+        self.segments = []
+        self.required_headers = [
+            'issuer',
+            'username',
+            'project',
+            ]
+        self.ingests = {
+            "tsv": self.ingest_tsv
+        }
+
+    def db_setup(self):
+        """
+        Connects to the database given using SQLAlchemy Core and
+        attempts to query the username provided. Returns the
+        engine if successful or an error if the Sample cannot
+        be located.
+
+        :raises: KeyExistenceError
+        :return: engine object
+        :rtype: `~sqlalchemy.engine.Engine`
+        """
+        pass
+
+        # init_db(self.db)
+        # engine = get_engine()
+        # with engine.connect() as connection:
+        #     result = connection.execute(
+        #         "select * from authorization where username=\"{}\""
+        #         .format(self.authorization)).first()
+        #     if self.patient != result[1]:
+        #         raise KeyExistenceError(self.sample)
+        # return engine
+
+    def ingest_tsv(self):
+        """
+        Ingest the candig-server v1 compatible tsv file to populate authorization info.
+
+        :raises: HeaderError
+        :return: None
+        """
+        # TODO: Use existing TSV files to population authz info.
+        pass
+
+
+    def upload(self):
+        """
+
+        :raises: IntegrityError
+        :return
+        """
+        pass
