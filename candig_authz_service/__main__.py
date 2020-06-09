33f81a96a8462cb7518587ae5bd26ffaeea57370
Felipe <fcoralsasso@bcgsc.ca>
2020-06-09 14:12:37 -0700

    Added Prometheus

--- candig_authz_service/__main__.py
+++ candig_authz_service/__main__.py
@@ -6,6 +6,7 @@ import logging
 import pkg_resources
 
 import connexion
+from prometheus_flask_exporter import PrometheusMetrics
 
 # from tornado.options import define
 # import candig_authz_service.orm
@@ -91,6 +92,7 @@ def configure_app():
 app = configure_app()
 
 application = app.app
+metrics = PrometheusMetrics(application)
 
 if __name__ == "__main__":
     main()
